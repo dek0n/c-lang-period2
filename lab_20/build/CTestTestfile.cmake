1673e709046d3bda6a6f44d2eb0d49e5f22fe82b
dek0n <dv.knnv@gmail.com>
2023-11-14 11:14:59 +0200

    m

--- lab_20/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dek0n/projects/c-lang-period2/lab_2
-# Build directory: /home/dek0n/projects/c-lang-period2/lab_2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
