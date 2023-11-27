c22868128c8ddc1b178aacfa19e1bc803c42e525
dek0n <dv.knnv@gmail.com>
2023-11-27 10:46:26 +0200

    m

--- lab_4/main.c
+++ lab_4/main.c
@@ -1,32 +1,23 @@
 #include "my_pico_definitions.h"
 
-// #define MY_I2C_BAUD_RATE 100000
-// int main()
-// {
-//     stdio_init_all();
-//     initialize_all_sw_buttons();
-//     initialize_all_leds();
-//     initialize_pwm_pin(PIN_LED3);
-//     i2c_init(i2c0, MY_I2C_BAUD_RATE); // Either i2c0 or i2c1, baudrate	Baudrate in Hz (e.g. 100kHz is 100000) Returns Actual set baudrate
-// }
+#define MY_I2C_BAUD_RATE 100000 // Baudrate of I2C in Hz (e.g. 100kHz is 100000)
+#define I2C0_SDA_PIN 16
+#define I2C0_SCL_PIN 17
+
+#define DEVADDR 0xA0
+
+uint8_t data_to_write = 0xA5;
+uint8_t *ptr_data_to_write = &data_to_write;
+uint8_t data_read;
 
 int main()
 {
-    const uint led_pin = 22;
-    uint count = 0;
-    gpio_init(led_pin);
-    gpio_set_dir(led_pin, GPIO_OUT);
     stdio_init_all();
-    cyw43_arch_init();
+    initialize_all_sw_buttons();
+    initialize_all_leds();
+    i2c_init(i2c0, MY_I2C_BAUD_RATE); // Either i2c0 or i2c1, baudrate in HZ, Returns Actual set baudrate
+    gpio_set_function(I2C0_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(I2C0_SCL_PIN, GPIO_FUNC_I2C);
 
-    while (true)
-    {
-        printf("Blink! %u\r\n", ++count);
-        // gpio_put(led_pin, true);
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
-        sleep_ms(1000);
-        // gpio_put(led_pin, false);
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-        sleep_ms(1000);
-    }
+    i2c_write_blocking(i2c0, DEVADDR, ptr_data_to_write, 2, false);
 }
