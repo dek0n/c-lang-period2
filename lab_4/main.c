620cd7602ce5631aab510c1f1535619688a1340c
dek0n <dv.knnv@gmail.com>
2023-11-27 15:28:33 +0200

    something is working

--- lab_4/main.c
+++ lab_4/main.c
@@ -4,11 +4,7 @@
 #define I2C0_SDA_PIN 16
 #define I2C0_SCL_PIN 17
 
-#define DEVADDR 0xA0
-
-uint8_t data_to_write = 0xA5;
-uint8_t *ptr_data_to_write = &data_to_write;
-uint8_t data_read;
+#define DEVADDR 0x50
 
 int main()
 {
@@ -19,5 +15,27 @@ int main()
     gpio_set_function(I2C0_SDA_PIN, GPIO_FUNC_I2C);
     gpio_set_function(I2C0_SCL_PIN, GPIO_FUNC_I2C);
 
-    i2c_write_blocking(i2c0, DEVADDR, ptr_data_to_write, 2, false);
+    uint8_t data_to_write[3] = {};
+    const uint16_t memory_slot = 0x7FFF;
+
+    // Divide into two 8-bit parts
+    data_to_write[0] = memory_slot & 0xFF;
+    data_to_write[1] = (memory_slot >> 8) & 0xFF;
+    data_to_write[2] = 0xFF;
+
+    uint8_t data_read;
+
+    i2c_write_blocking(i2c0, DEVADDR, data_to_write, 3, false);
+    sleep_ms(5);
+
+    i2c_read_blocking(i2c0, DEVADDR, &data_read, 3, false);
+
+    if (data_read == data_to_write[2])
+    {
+        printf("Write and read successful.\n");
+    }
+    else
+    {
+        printf("Error: Read value does not match the written value.\n");
+    }
 }
