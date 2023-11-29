b3ee3c2f2fce823f0e245bd16f959eb190b1898b
dek0n <dv.knnv@gmail.com>
2023-11-29 14:12:18 +0200

    multibyte read/write ok

--- lab_4/main.c
+++ lab_4/main.c
@@ -14,28 +14,40 @@ int main()
     i2c_init(i2c0, MY_I2C_BAUD_RATE); // Either i2c0 or i2c1, baudrate in HZ, Returns Actual set baudrate
     gpio_set_function(I2C0_SDA_PIN, GPIO_FUNC_I2C);
     gpio_set_function(I2C0_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C0_SDA_PIN);
+    gpio_pull_up(I2C0_SCL_PIN);
 
-    uint8_t data_to_write[3] = {};
-    const uint16_t memory_slot = 0x7FFF;
+    uint8_t data_to_write[4] = {};
+    const uint16_t memory_slot = 0x0100;
 
     // Divide into two 8-bit parts
-    data_to_write[0] = memory_slot & 0xFF;
-    data_to_write[1] = (memory_slot >> 8) & 0xFF;
-    data_to_write[2] = 0xFF;
+    data_to_write[0] = (memory_slot >> 8) & 0xFF;
+    data_to_write[1] = memory_slot & 0xFF;
+    data_to_write[2] = 0x18;
+    data_to_write[3] = 0x17;
 
-    uint8_t data_read;
+    uint8_t data_read[2];
 
-    i2c_write_blocking(i2c0, DEVADDR, data_to_write, 3, false);
-    sleep_ms(5);
+    i2c_write_blocking(i2c0, DEVADDR, data_to_write, 4, false);
+    sleep_ms(10);
 
-    i2c_read_blocking(i2c0, DEVADDR, &data_read, 3, false);
+    i2c_write_blocking(i2c0, DEVADDR, data_to_write, 2, false);
+    i2c_read_blocking(i2c0, DEVADDR, data_read, 2, false);
+    printf("%s\n", "------");
+    printf("%02X\n", data_read[0]);
+    printf("%02X\n", data_read[1]);
+    printf("%s\n", "      ");
 
-    if (data_read == data_to_write[2])
+
+    for (size_t i = 0; i < 2; i++)
     {
-        printf("Write and read successful.\n");
-    }
-    else
-    {
-        printf("Error: Read value does not match the written value.\n");
+        if (data_read[i] == data_to_write[i + 2])
+        {
+            printf("Write and read successful.\n");
+        }
+        else
+        {
+            printf("Error: Read value does not match the written value.\n");
+        }
     }
 }
