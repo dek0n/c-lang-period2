b24ac48efc85a76cebf8c737c22494431d03c4c8
dek0n <dv.knnv@gmail.com>
2023-12-07 23:21:18 +0200

    some 5 lab

---
+++ lab_5_1/main.c
@@ -0,0 +1,115 @@
+#include "my_pico_definitions.h"
+
+// I2C Configuration
+#define MY_I2C_BAUD_RATE 100000
+#define I2C0_SDA_PIN 16
+#define I2C0_SCL_PIN 17
+
+// EEPROM Configuration
+#define DEVADDR 0x50
+#define MEMORY_ADDR_LED_STATES 0x7FFE // Second-highest memory address in EEPROM
+
+// Constants for Bitwise Operations
+#define MASK_8B_ALL1 0xFF
+
+// For input
+#define ASCII_SPACE 32
+#define ASCII_ENTER 13
+#define ASCII_BACKSPACE 8
+#define MY_INPUT_DEALY_MS 100
+#define MY_STRING_LENGTH_MAX 10
+
+#define STRING_STATUS "status"
+#define STRING_CALIB "calib"
+#define STRING_RUN "run "
+
+int main()
+{
+
+    // Initialization
+    stdio_init_all();
+    initialize_all_sw_buttons();
+    initialize_all_leds();
+
+    // I2C Initialization
+    i2c_init(i2c0, MY_I2C_BAUD_RATE);
+    gpio_set_function(I2C0_SDA_PIN, GPIO_FUNC_I2C);
+    gpio_set_function(I2C0_SCL_PIN, GPIO_FUNC_I2C);
+    gpio_pull_up(I2C0_SDA_PIN);
+    gpio_pull_up(I2C0_SCL_PIN);
+
+    char my_character;
+    int my_character_int;
+    char my_character_array[MY_STRING_LENGTH_MAX];
+    bool my_input_loop = false;
+    int my_input_index = 0;
+    int my_number_of_runs = 0;
+
+    printf("Press ENTER for input mode.\n");
+    // MAIN LOOP
+    while (true)
+    {
+        my_character = my_character_int = getchar_timeout_us(0);
+        if (my_character_int == ASCII_ENTER)
+        {
+            printf("INPUT MODE:");
+            my_input_loop = true;
+            while (my_input_loop)
+            {
+                my_character = my_character_int = getchar_timeout_us(0);
+                if (my_character_int != PICO_ERROR_TIMEOUT)
+                {
+                    if (my_character_int == ASCII_ENTER)
+                    {
+                        my_character_array[my_input_index] = '\0';
+                        printf("\n");
+                        if (!strcmp(my_character_array, "status"))
+                        {
+                            printf("STATUS...\n");
+                        }
+                        else if (!strcmp(my_character_array, "calib"))
+                        {
+                            printf("CALIBRATION...\n");
+                        }
+                        else if (!strcmp(my_character_array, "run"))
+                        {
+                            printf("RUN...\n");
+                        }
+                        else if (!strncmp(my_character_array, "run", strlen("run")))
+                        {
+                            if (sscanf(my_character_array + strlen("run"), " %d", &my_number_of_runs))
+                            {
+                                printf("String starts with 'run' and the integer is: %d\n", my_number_of_runs);
+                            }
+                        }
+                        else
+                        {
+                            printf("INPUT ERROR! Available commands:'status','run N'(N - number).\n");
+                            printf("Press ENTER for input mode.\n");
+                        }
+                        // reset_input_array(my_character_array, MY_STRING_LENGTH_MAX);
+                        my_input_index = 0;
+                        my_input_loop = false;
+                    }
+
+                    else if (my_character_int == ASCII_BACKSPACE && my_input_index > 0)
+                    {
+                        my_character_array[my_input_index] = ' ';
+                        my_input_index--;
+                        printf("\b \b");
+                    }
+
+                    else
+                    {
+                        if (my_character_int != ASCII_BACKSPACE && my_input_index < MY_STRING_LENGTH_MAX)
+                        {
+                            my_character_array[my_input_index] = my_character;
+                            my_input_index++;
+                            printf("%c", (char)my_character);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ lab_5_1/my_pico_definitions.h
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <ctype.h>
+
+#include "pico/stdlib.h"
+#include "pico/time.h"
+#include "hardware/irq.h"
+#include "hardware/gpio.h"
+#include "hardware/pwm.h"
+#include "hardware/uart.h"
+#include "hardware/i2c.h"
+#include "pico/cyw43_arch.h"
+
+// Pins
+#define PIN_LED1 22
+#define PIN_LED2 21
+#define PIN_LED3 20
+
+#define PIN_SW0 9
+#define PIN_SW1 8
+#define PIN_SW2 7
+
+#define PIN_ROTA 10
+#define PIN_ROTB 11
+#define PIN_ROT_Push 12
+
+// For PWM
+#define PWM_CHANNEL_TOP 999         // +1 Determines frequency?
+#define PWM_CHANNEL_COMPARE_LEVEL 0 // Initial compare level of a channel (determines initial brightness)
+#define PWM_CLOCK_DIVIDER 125       // Initial PWM 125Mhz/125 will give 1MHz
+
+// I2C Configuration
+#define MY_I2C_BAUD_RATE 100000
+#define I2C0_SDA_PIN 16
+#define I2C0_SCL_PIN 17
+
+// EEPROM Configuration
+#define DEVADDR 0x50
+#define MEMORY_ADDR_LED_STATES 0x7FFE // Second-highest memory address in EEPROM
+
+// Constants for Bitwise Operations
+#define MASK_8B_ALL1 0xFF
+
+// Other
+#define MAX_LED_BRIGHTNESS (PWM_CHANNEL_TOP + 1) // max compare level of the PIN's PWM from 0 to PWM_COUNTER_WRAP
+
+struct SwitchStates // structure for sw buttons states
+{
+    int switch0;
+    int switch1;
+    int switch2;
+
+    bool sw_changed;
+
+    bool sw_all_toggle;
+
+    int rot_push;
+    int rot_turn_direction; // 0 counterclock, 1 clockwise
+    bool rot_turn_toggle;
+};
+
+typedef struct LedStates // structure for led states
+{
+    uint8_t state;
+    uint8_t not_state;
+} ledstate;
+
+// Function initializations
+void initialize_pwm_pin(int pin_for_pwm); // maybe change name
+bool rt_callback_function_sw(struct repeating_timer *rt);
+void initialize_all_leds();
+void initialize_all_sw_buttons();
+void all_leds_on(int led_brightness);
+void all_leds_off();
+void process_DevEui(const char *devEuiWithPrefix, char *result);
+void clear_uart_buffer(uart_inst_t *uart);
+void print_time_stamp_s();
+void print_led_states(ledstate *ls);
+bool led_state_is_valid(ledstate *ls);
+void set_led_state(ledstate *ls, uint8_t value);
+void print_binary(uint8_t value);
+void update_leds_from_led_states(ledstate *ls);
+
+// void my_interrupt_handler(); // this function is currently in main.c
+
+// GPIO        00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15
+// PWM Channel 0A 0B 1A 1B 2A 2B 3A 3B 4A 4B 5A 5B 6A 6B 7A 7B , A = channel 0, B = channel 1
+
+// GPIO        15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30
+// PWM Channel 0A 0B 1A 1B 2A 2B 3A 3B 4A 4B 5A 5B 6A 6B 7A 7B , A = channel 0, B = channel 1
