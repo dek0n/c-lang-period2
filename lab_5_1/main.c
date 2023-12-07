aa4dd4e5eef47374851738bbb47e8c0e7b01cffc
dek0n <dv.knnv@gmail.com>
2023-12-08 00:42:38 +0200

    input by interrupt + addes ESC

--- lab_5_1/main.c
+++ lab_5_1/main.c
@@ -16,12 +16,22 @@
 #define ASCII_SPACE 32
 #define ASCII_ENTER 13
 #define ASCII_BACKSPACE 8
+#define ASCII_ESC 27
 #define MY_INPUT_DEALY_MS 100
 #define MY_STRING_LENGTH_MAX 10
 
 #define STRING_STATUS "status"
 #define STRING_CALIB "calib"
 #define STRING_RUN "run "
+
+bool my_input_toggle = false;
+void my_interrupt_handler()
+{
+    if (!my_input_toggle)
+    {
+        my_input_toggle = true;
+    }
+}
 
 int main()
 {
@@ -41,70 +51,73 @@ int main()
     char my_character;
     int my_character_int;
     char my_character_array[MY_STRING_LENGTH_MAX];
-    bool my_input_loop = false;
     int my_input_index = 0;
     int my_number_of_runs = 0;
 
-    printf("Press ENTER for input mode.\n");
+    // Interrupts
+    gpio_set_irq_enabled_with_callback(1, GPIO_IRQ_EDGE_RISE, true, &my_interrupt_handler);
+
+    printf("\nStart typing for input mode. Available commands: 'status', 'calib', 'run N'. (N - number)\n");
     // MAIN LOOP
     while (true)
     {
-        my_character = my_character_int = getchar_timeout_us(0);
-        if (my_character_int == ASCII_ENTER)
+        while (my_input_toggle)
         {
-            printf("INPUT MODE:");
-            my_input_loop = true;
-            while (my_input_loop)
+            my_character = my_character_int = getchar_timeout_us(5);
+            if (my_character_int != PICO_ERROR_TIMEOUT)
             {
-                my_character = my_character_int = getchar_timeout_us(0);
-                if (my_character_int != PICO_ERROR_TIMEOUT)
+                if (my_character_int == ASCII_ENTER)
                 {
-                    if (my_character_int == ASCII_ENTER)
+                    my_character_array[my_input_index] = '\0';
+                    printf("\n");
+                    if (!strcmp(my_character_array, "status"))
                     {
-                        my_character_array[my_input_index] = '\0';
-                        printf("\n");
-                        if (!strcmp(my_character_array, "status"))
-                        {
-                            printf("STATUS...\n");
-                        }
-                        else if (!strcmp(my_character_array, "calib"))
-                        {
-                            printf("CALIBRATION...\n");
-                        }
-                        else if (!strcmp(my_character_array, "run"))
-                        {
-                            printf("RUN...\n");
-                        }
-                        // !!! Modify next not to accept with other symbol after number)
-                        else if (!strncmp(my_character_array, "run ", strlen("run ")) && sscanf(my_character_array + strlen("run "), "%d", &my_number_of_runs))
-                        {
-                            printf("RUN...%d times\n", my_number_of_runs);
-                        }
-                        else
-                        {
-                            printf("INPUT ERROR! Available commands:'status', 'calib', 'run N'. (N - number)\n");
-                        }
-                        printf("\nPress ENTER for input mode.\n");
-                        // reset_input_array(my_character_array, MY_STRING_LENGTH_MAX);
-                        my_input_index = 0;
-                        my_input_loop = false;
+                        printf("STATUS...\n");
                     }
-
-                    else if (my_character_int == ASCII_BACKSPACE && my_input_index > 0)
+                    else if (!strcmp(my_character_array, "calib"))
                     {
-                        my_character_array[my_input_index] = ' ';
-                        my_input_index--;
-                        printf("\b \b");
+                        printf("CALIBRATION...\n");
                     }
-
+                    else if (!strcmp(my_character_array, "run"))
+                    {
+                        printf("RUN...\n");
+                    }
+                    // !!! Modify next not to accept with other symbol after number)
+                    else if (!strncmp(my_character_array, "run ", strlen("run ")) && sscanf(my_character_array + strlen("run "), "%d", &my_number_of_runs))
+                    {
+                        printf("RUN...%d times\n", my_number_of_runs);
+                    }
                     else
                     {
-                        if (my_character_int != ASCII_BACKSPACE && my_input_index < MY_STRING_LENGTH_MAX)
-                        {
-                            my_character_array[my_input_index] = my_character;
-                            my_input_index++;
-                            printf("%c", (char)my_character);
-                        }
+                        printf("INPUT ERROR!\n");
+                    }
+                    printf("\nStart typing for input mode. Available commands: 'status', 'calib', 'run N'. (N - number)\n");
+                    my_input_index = 0;
+                    my_input_toggle = false;
+                }
+
+                else if (my_character_int == ASCII_ESC)
+                {
+                    printf("\nExited from input mode.\n");
+                    printf("\nStart typing for input mode. Available commands: 'status', 'calib', 'run N'. (N - number)\n");
+                    my_input_index = 0;
+                    my_input_toggle = false;
+                }
+
+                else if (my_character_int == ASCII_BACKSPACE && my_input_index > 0)
+                {
+                    my_character_array[my_input_index] = ' ';
+                    my_input_index--;
+                    printf("\b \b");
+                }
+
+                else
+                {
+                    if (my_character_int != ASCII_BACKSPACE && my_input_index < MY_STRING_LENGTH_MAX)
+                    {
+                        my_character_array[my_input_index] = my_character;
+                        my_input_index++;
+                        printf("%c", (char)my_character);
                     }
                 }
             }
